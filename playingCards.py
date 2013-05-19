60612278ba5f9fdbb5ef18842d0492dd4ac29d99
Diamond Bishop <diamondbishop@gmail.com>
2013-05-19 03:16:40 -0400

    graphics added

--- playingCards.py
+++ playingCards.py
@@ -1,5 +1,10 @@
+import pygame
+from pygame.locals import *
+
 suits = ['diamond', 'spade', 'heart', 'club']
 names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+green = pygame.Color(0, 255, 0)
+white = pygame.Color(255, 255, 255)
 
 class Card():
 	def __init__(self, suitVal, nameVal):
@@ -11,6 +16,9 @@ class Card():
 
 	def name(self):
 		return names[self.nameVal]
+
+	#def displayCard(x, y, hide=False):
+		
 
 	def __str__(self):
 		return "%s of %ss" % (self.name(), self.suit())
@@ -29,3 +37,30 @@ if __name__ == '__main__':
 	deck = Deck()
 	for card in deck.getCards():
 		print card
+	
+	pygame.init()
+	fpsClock = pygame.time.Clock()
+
+	#display board
+	windowSurfaceObj = pygame.display.set_mode((640, 480))
+	pygame.display.set_caption('Heads Up!')
+	
+	while True:
+		windowSurfaceObj.fill(green)
+		
+		#draw card outlines
+		pygame.draw.rect(windowSurfaceObj, white, (10, 10, 50, 100))
+		
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			elif event.type == MOUSEMOTION:
+				mousex, mousey = event.pos
+			elif event.type == MOUSEBUTTONUP:
+				mousex, mousey = event.pos
+				if event.button == 1: #left click
+					msg = 'left click'
+	
+		pygame.display.update()
+		fpsClock.tick(30)
